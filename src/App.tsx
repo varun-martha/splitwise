d8471c8cec5c2fd0ec7770c8fe2f60e2c77d79d9
varun-martha <varunkumar.martha@everest.engineering>
2024-12-13 20:09:44 +0530

    Add routes and elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import WelcomePage from './pages/welcome/WelcomePage.tsx';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/login/LoginPage.tsx';
+import RegisterPage from './pages/register/RegisterPage.tsx';
+import HomePage from './pages/home/HomePage.tsx';
 
 function App() {
   return (
-    <div>
-      <p>Welcome to splitwise web app!</p>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<WelcomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/home' element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
