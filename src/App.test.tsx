d0061ce10fbec79d51b6dc639b2d3becfd4176e2
varun-martha <varunkumar.martha@everest.engineering>
2024-12-16 11:40:15 +0530

    Modify import statements

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App.tsx';
+import App from './App';
 import { InitialEntry } from '@remix-run/router';
 import { MemoryRouter } from 'react-router-dom';
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import WelcomePage from './pages/welcome/WelcomePage.tsx';
+import WelcomePage from './pages/welcome/WelcomePage';
 import { Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/login/LoginPage.tsx';
-import RegisterPage from './pages/register/RegisterPage.tsx';
-import HomePage from './pages/home/HomePage.tsx';
+import LoginPage from './pages/login/LoginPage';
+import RegisterPage from './pages/register/RegisterPage';
+import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
