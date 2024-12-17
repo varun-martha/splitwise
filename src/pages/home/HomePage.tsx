926e13e13522228ccf5dd9cdebd3b174eb76ee50
varun-martha <varunkumar.martha@everest.engineering>
2024-12-17 16:27:23 +0530

    Add title, add expense button, invite friend button and settle up button with tag line

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <div>Welcome HomePage</div>
+        <div>
+            <h1>Splitwise application!</h1>
+            <p>Splitwise helps you split bills with friends.</p>
+            <p>Click “Add an expense” above to get started, or invite some friends first!</p>
+            <button>Add expense</button>
+            <button>Invite friend</button>
+            <button>Settle up</button>
+        </div>
     )
 }
 
