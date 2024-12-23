dfcf0a1f25fb17fef169d0cd0d18e29a7ceaf92f
varun-martha <varunkumar.martha@everest.engineering>
2024-12-23 14:49:06 +0530

    Modify homepage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,16 +1,85 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar.tsx";
+import { Button, Typography, List, ListItem } from "@mui/material";
+import './HomePage.css'; 
+import AddFriend from "../../components/AddFriend/AddFriend.tsx";
+import InviteFriend from "../../components/InviteFriend/InviteFriend.tsx";
+import { GlobalContext } from "../../context/context.tsx";
 
 const HomePage = () => {
-    return (
-        <div>
-            <h1>Splitwise application!</h1>
-            <p>Splitwise helps you split bills with friends.</p>
-            <p>Click “Add an expense” above to get started, or invite some friends first!</p>
-            <button>Add expense</button>
-            <button>Invite friend</button>
-            <button>Settle up</button>
+    const {user} = useContext(GlobalContext);
+    const [openAddFriendModal, setAddFriendModal] = useState(false);
+    const [openInvite, setOpenInvite] = useState(false);
+    const [friends, setFriends] = useState<string[]>([]);
+
+    const fetchFriends = async() => {
+      try{
+        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/${user.username}/friends`, {
+          method: 'GET'
+        });
+        const result = await response.json();
+        if(response.ok) {
+          setFriends(result.friends);
+        }
+      } catch(err) {
+        alert("Error ocurred");
+      }
+    }
+
+    useEffect(() => {
+      fetchFriends();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="box2">
+        <div className="left-block">
+          <div className="button-wrapper">
+            <Button
+              variant="contained"
+              className="invite-friend-btn"
+              onClick={() => setOpenInvite(true)}
+            >
+              Invite Friend
+            </Button>
+          </div>
+          <div className="button-wrapper">
+            <Button
+              variant="contained"
+              className="add-friend-btn"
+              onClick={() => setAddFriendModal(true)}
+            >
+              Add Friend
+            </Button>
+          </div>
         </div>
-    )
-}
+        <div className="middle-block">
+          <div className="button-wrapper">
+            <Button variant="contained" color="primary">
+              Add Expense
+            </Button>
+          </div>
+          <div className="button-wrapper">
+            <Button variant="outlined" color="secondary">
+              Clear Expense
+            </Button>
+          </div>
+        </div>
+        <div className="right-block">
+          <Typography variant="h6">Your Friends</Typography>
+          <List>
+            {friends.length !== 0 && friends.map((friend) => {
+              return <ListItem key={friend}>{friend}</ListItem>
+            })}
+          </List>
+        </div>
+      </div>
+      <AddFriend open={openAddFriendModal} closeModal={setAddFriendModal} fetchFriends={fetchFriends}/>
+      <InviteFriend openInvite={openInvite} closeModal={setOpenInvite}/>
+    </div>
+  );
+};
 
 export default HomePage;
