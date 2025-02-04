55e29dff7e9f6cfc0586877d1843222d5485c750
varun-martha <varunkumar.martha@everest.engineering>
2025-02-04 11:57:22 +0530

    Modify home page test cases

--- src/pages/home/HomePage.test.tsx
+++ src/pages/home/HomePage.test.tsx
@@ -1,11 +1,22 @@
 import { screen, render } from "@testing-library/react";
 import HomePage from "./HomePage";
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
 
 describe('tests related to the Homepage', () => {
 
+    const renderHomePage = () => {
+        return render(
+            <MemoryRouter>
+                <HomePage />
+            </MemoryRouter>
+        )
+    }
+
     test('should render Homepage correctly', () => {
-        render(<HomePage />);
-        expect(screen.getByText(/WElCOME HOMEPAGE/i)).toBeInTheDocument();
+        renderHomePage();
+        
+        expect(screen.getByText(/Hello/i)).toBeInTheDocument();
+        expect(screen.getByText(/sign out/i)).toBeInTheDocument();
     });
 })
